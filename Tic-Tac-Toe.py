ed9df2b4b80bfb76536b4817c88f96a45df0593f
ghostGelus <68477118+ghostGelus@users.noreply.github.com>
2020-07-19 00:28:30 +0700

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -8,3 +8,21 @@ def printBoard(board):
     print(board['4'] + '|' + board['5'] + '|' + board['6'])
     print('-+-+-')
     print(board['1'] + '|' + board['2'] + '|' + board['3'])
+
+def game():
+
+    turn = 'X'
+    count = 0
+    
+    for i in range(10):
+            printBoard(theBoard)
+            print("It's your turn," + turn + ".Move to which place?")
+
+            move = input()        
+
+            if theBoard[move] == ' ':
+                theBoard[move] = turn
+                count += 1
+            else:
+                print("That place is already filled.\nMove to which place?")
+                continue
