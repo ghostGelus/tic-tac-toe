8f79f351fd86dfd334a4a23ea975238b9c89cf03
ghostGelus <68477118+ghostGelus@users.noreply.github.com>
2020-07-19 00:29:18 +0700

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -79,3 +79,17 @@ def game():
             turn = 'O'
         else:
             turn = 'X' 
+
+board_keys = []
+
+for key in theBoard:
+    board_keys.append(key)
+    
+restart = input("Do want to play Again?(y/n)")
+
+    if restart == "y" or restart == "Y":  
+        for key in board_keys:
+            theBoard[key] = " "
+
+        game()    
+           
